f74a23144fad4659acec21867230bc46e872558e
RanaM <rana_093@yahoo.com>
2024-09-13 19:55:55 +0600

    Refactored the LC Daily challenges!

--- Leetcode/1684.go
+++ Leetcode/1684.go
@@ -1,24 +1,18 @@
 package Leetcode
 
-func isConsistent(mp map[rune]bool, str string) bool {
-	for _, ch := range str {
-		if !mp[ch] {
-			return false
-		}
-	}
-	return true
-}
+import "strings"
 
 func countConsistentStrings(allowed string, words []string) int {
-	presence := make(map[rune]bool)
-	for _, ch := range allowed {
-		presence[ch] = true
-	}
 	ans := 0
 	for i := 0; i < len(words); i++ {
-		if isConsistent(presence, words[i]) {
-			ans++
+		for _, ch := range words[i] {
+			res := strings.ContainsRune(allowed, ch)
+			if !res {
+				ans--
+				break
+			}
 		}
+		ans++
 	}
 	return ans
 }
