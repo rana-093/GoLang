3302caeb6e94eece77c3f3e66600347800b63b48
RanaM <rana_093@yahoo.com>
2024-09-16 20:41:39 +0600

    Solved the LC Daily challenges - 539!

---
+++ Leetcode/539.go
@@ -0,0 +1,28 @@
+package Leetcode
+
+import (
+	"math"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func findMinDifference(timePoints []string) int {
+	var arr []int
+	for _, val := range timePoints {
+		parts := strings.Split(val, ":")
+		hour, _ := strconv.Atoi(parts[0])
+		minute, _ := strconv.Atoi(parts[1])
+		totalMinutes := 60*hour + minute
+		arr = append(arr, totalMinutes)
+		totalMinutesIn24HrFormat := 60*(hour+24) + minute
+		arr = append(arr, totalMinutesIn24HrFormat)
+	}
+	sort.Ints(arr)
+	ans := math.MaxInt
+	for i := 0; i+1 < len(arr); i++ {
+		diff := arr[i+1] - arr[i]
+		ans = min(ans, diff)
+	}
+	return ans
+}
