12b2c311cb9e4f04de4149c54389dc95d9aabccb
m.rana <m.rana@shamolima.com>
2024-09-29 13:07:46 +0600

    Added the script for calculating positive and negative acc

--- Backend/main.go
+++ Backend/main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 	"io"
+	"log"
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/jackc/pgx/v4"
 )
 
 const (
 	region = "ap-south-1"
 	bucket = "report-sl"
 )
+
+type TravelMetaData struct {
+	DeviceId            string
+	From                time.Time
+	To                  time.Time
+	ObjectHistoryUrl    string
+	DailyUsageReportUrl string
+	CompanyId           *string
+	TrackerId           *string
+	MonthOfReport       string
+}
 
 type VehicleReport struct {
 	TotalAcceleration    int
@@ -76,11 +92,79 @@ func calculateObjectHistoryForGettingAccAndDcc(svc *s3.S3, key string) map[strin
 	return accAndDccReport
 }
 
+func saveReportToDB(vehicleReport []VehicleReport, travelMetaData TravelMetaData) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+	fmt.Printf("dsn is %s\n", dsn)
+	conn, err := pgx.Connect(context.Background(), dsn)
+	if err != nil {
+		log.Fatalf("Unable to connect to the database: %v\n", err)
+	}
+	defer conn.Close(context.Background())
+
+	fmt.Println("Successfully connected to the database!")
+
+	for _, report := range vehicleReport {
+		query := `INSERT INTO cargo_reports 
+    			(harsh_acc_count, harsh_dcc_count, acc_per_100_km, 
+    			 dcc_per_100_km, distance, from_date, to_date, 
+    			 device_id, month_of_report) 
+				VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);`
+		_, err := conn.Exec(
+			context.Background(),
+			query,
+			report.TotalAcceleration,
+			report.TotalDeceleration,
+			report.AccelerationPer100KM,
+			report.DecelerationPer100KM,
+			report.TotalDistanceCovered,
+			travelMetaData.From,
+			travelMetaData.To,
+			travelMetaData.DeviceId,
+			travelMetaData.MonthOfReport,
+		)
+		if err != nil {
+			log.Fatalf("Error executing insert query: %v\n", err)
+		}
+	}
+
+	var greeting string
+	err = conn.QueryRow(context.Background(), "SELECT 'Hello, PostgreSQL!'").Scan(&greeting)
+	if err != nil {
+		log.Fatalf("QueryRow failed: %v\n", err)
+	}
+
+	fmt.Println(greeting)
+}
+
 func handleDownLoadFile(w http.ResponseWriter, r *http.Request) {
 	objectHistoryUrl := r.URL.Query().Get("objectHistoryUrl")
 	dailyUsageUrl := r.URL.Query().Get("dailyUsageUrl")
+	fromDate := r.URL.Query().Get("fromDate")
+	toDate := r.URL.Query().Get("toDate")
+	deviceId := r.URL.Query().Get("deviceId")
+	trackerId := r.URL.Query().Get("trackerId")
 
-	slog.Info("Object History URL is: ", objectHistoryUrl, " and daily usage url is: ", dailyUsageUrl)
+	layout := "2006-01-02"
+	parsedFromDate, err := time.Parse(layout, fromDate)
+	if err != nil {
+		_ = fmt.Errorf("Error parsing fromDate: %v\n", err)
+	}
+	parsedToDate, err := time.Parse(layout, toDate)
+	if err != nil {
+		_ = fmt.Errorf("Error parsing toDate: %v\n", err)
+	}
+
+	slog.Info("Object History URL is: ", objectHistoryUrl, " and daily usage url is: ", dailyUsageUrl, " from date: ", fromDate, " to date: ", toDate)
 
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String(region),
@@ -125,12 +209,24 @@ func handleDownLoadFile(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 
-	response, err := json.Marshal(objectHistoryReport)
+	response, err := json.Marshal(vechicleReport)
 
 	if err != nil {
 		http.Error(w, "Failed to marshal response", http.StatusInternalServerError)
 		return
 	}
+
+	travelMetaData := TravelMetaData{
+		From:                parsedFromDate,
+		To:                  parsedToDate,
+		DeviceId:            deviceId,
+		ObjectHistoryUrl:    objectHistoryUrl,
+		DailyUsageReportUrl: dailyUsageUrl,
+		TrackerId:           &trackerId,
+		MonthOfReport:       parsedFromDate.Month().String(),
+	}
+
+	saveReportToDB(vechicleReport, travelMetaData)
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(response)
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/chromedp/chromedp v0.10.0
 	github.com/gocolly/colly/v2 v2.0.0
 	github.com/google/uuid v1.6.0
+	github.com/jackc/pgx/v4 v4.18.3
 	github.com/joho/godotenv v1.5.1
 	github.com/xuri/excelize/v2 v2.8.1
 )
@@ -24,6 +25,13 @@ require (
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.4.0 // indirect
 	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
